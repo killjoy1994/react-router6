a0a0d131b024217953987bc74e1ccf561f4141e2
BagusNugrohow <papaobobo@gmail.com>
2022-01-24 13:41:05 +0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+import { BrowserRouter as Router, Routes, Route, Navigate, Link } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <Routes>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/myapps" element={<Navigate replace to="/learn" />} />
+      <Route path="/learn" element={<Learn />}>
+        <Route path="courses" element={<Courses />}>
+          Courses
+        </Route>
+        <Route path="bundles" element={<Bundles />}>
+          Bundles
+        </Route>
+      </Route>
+      {/* <Route path="*" element={<Error />} /> */}
+    </Routes>
+  </Router>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+function Home() {
+  return (
+    <div>
+      <h1>Home route</h1>
+    </div>
+  );
+}
+
+function Learn() {
+  return (
+    <div>
+      <h1>Learn</h1>
+      <h4>All courses are listed here</h4>
+      <Link className="btn btn-success" to="/learn/courses">
+        Course
+      </Link>{" "}
+      <Link className="btn btn-primary" to="/learn/bundles">
+        Bundle
+      </Link>
+    </div>
+  );
+}
+
+function Courses() {
+  return (
+    <div>
+      <h1>Course List</h1>
+      <h4>Course Card</h4>
+    </div>
+  );
+}
+
+function Bundles() {
+  return (
+    <div>
+      <h1>Bundle List</h1>
+      <h4>Bundle Card</h4>
+    </div>
+  );
+}
+
+function Error() {
+  return (
+    <div>
+      <h1>Error Page</h1>
+    </div>
+  );
+}
+
 reportWebVitals();
